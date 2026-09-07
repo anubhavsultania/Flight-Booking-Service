0b09bbe3b7ed7ede452b83bb1479a503cc20fe33
anubhavsultania <122CH0539@nitrkl.ac.in>
2026-09-07 19:09:49 +0530

    Added booking payment logic

--- src/routes/v1/booking.js
+++ src/routes/v1/booking.js
@@ -3,4 +3,5 @@ const { BookingController } = require("../../controllers");
 const router = express.Router();
 
 router.post("/", BookingController.createBooking);
+router.post("/payments", BookingController.makePayment)
 module.exports = router;
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -4,7 +4,8 @@ const db = require("../models");
 const AppError = require("../utils/errors/app-error");
 const { StatusCodes } = require("http-status-codes");
 const { ServerConfig } = require("../config");
-
+const { Enums } = require("../utils/common");
+const { BOOKED, CANCELLED, INITIATED, PENDING } = Enums.BOOKING_STATUS;
 const bookingRepository = new BookingRepository();
 
 async function createBooking(data) {
@@ -36,4 +37,51 @@ async function createBooking(data) {
     throw error;
   }
 }
-module.exports = { createBooking };
+
+async function makePayment(data) {
+  const transaction = await db.sequelize.transaction();
+  try {
+    const bookingDetails = await bookingRepository.get(
+      data.bookingId,
+      transaction,
+    );
+    if (bookingDetails.status === CANCELLED) {
+      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+    }
+    const bookingTime = new Date(bookingDetails.createdAt);
+    const currentTime = new Date();
+    if (currentTime - bookingTime > 300000) {
+      await bookingRepository.update(
+        data.bookingId,
+        { status: CANCELLED },
+        transaction,
+      );
+      throw new AppError("The booking has expired", StatusCodes.BAD_REQUEST);
+    }
+    if (bookingDetails.totalCost != data.totalCost) {
+      throw new AppError(
+        "The amount of the payment doesn't match",
+        StatusCodes.BAD_REQUEST,
+      );
+    }
+    if (bookingDetails.userId != data.userId) {
+      throw new AppError(
+        "The user corresponding to the booking doesn't match",
+        StatusCodes.BAD_REQUEST,
+      );
+    }
+    const response = await bookingRepository.update(
+      data.bookingId,
+      {
+        status: BOOKED,
+      },
+      transaction,
+    );
+    await transaction.commit();
+  } catch (error) {
+    console.log(error);
+    await transaction.rollback();
+    throw error;
+  }
+}
+module.exports = { createBooking, makePayment };
